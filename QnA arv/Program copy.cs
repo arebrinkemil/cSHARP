60c0134b37c5fc15a6bf5b31007b2ad245951b6d
arebrinkemil <earebrink@gmail.com>
2020-11-02 11:22:48 +0100

    klart

---
+++ QnA arv/Program copy.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Questions
+{
+
+
+      class QuestionChoice : Questions12
+    {
+        String F1;
+        String F2;
+        String F3;
+        public QuestionChoice( String pq, String pa, String pa1, String pa2, String pa3)
+        {
+            question = pq;
+            answer = pa;
+            F1 = pa1;
+            F2 = pa2;
+            F3 = pa3;
+           
+        }
+
+        public override void Skrivut()
+        {
+            Console.WriteLine(question);
+            Console.WriteLine("");
+            Console.WriteLine("1. " + F1);
+            Console.WriteLine("");
+            Console.WriteLine("X. " + F2);
+            Console.WriteLine("");
+            Console.WriteLine("2. " + F3);
+            Console.WriteLine("");
+   
+            
+        }
+    }
+
+
+
+        class QuestionText : Questions12
+    {
+
+        public QuestionText( String q1, String q2)
+        {
+            question = q1;
+            answer = q2;
+        }
+
+        public override void Skrivut()
+        {
+            Console.WriteLine(question);
+        }
+    }
+
+
+
+
+
+
+
+  
+
+}
+
+
+
+
--- QnA arv/Program.cs
+++ QnA arv/Program.cs
@@ -1,12 +1,97 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace QnA_arv
+namespace Questions
 {
+    
+     abstract class Questions12
+    {
+        protected String question;
+
+        protected String answer;
+
+    
+         public void GetAnswer(Questions12 svaret)
+                {
+                    String SVAR = Console.ReadLine();
+                    if (SVAR == svaret.answer)
+                    {
+                        Console.WriteLine("Rätt!");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Fel!");
+                    }
+    
+                }
+        abstract public void Skrivut();    
+        }
+
+
     class Program
     {
+
+       static List<Questions12> fragor = new List<Questions12>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            
+                
+                fragor.Add(new QuestionText("Vad heter Norges1 huvudstad?", "Oslo"));
+               
+                fragor.Add(new QuestionChoice("Vad heter Norges2 huvudstad?","2", "Stockholm", "Lissabon", "Oslo"));
+
+                fragor.Add(new QuestionChoice("Vad heter Norges3 huvudstad?","2", "Stockholm", "Lissabon", "Oslo"));
+
+                fragor.Add(new QuestionChoice("Vad heter Norges4 huvudstad?","2", "Stockholm", "Lissabon", "Oslo"));
+
+                fragor.Add(new QuestionChoice("Vad heter Norges5 huvudstad?","2", "Stockholm", "Lissabon", "Oslo"));
+                
+                fragor.Add(new QuestionText("Vad heter sveriges6 huvudstad?", "Stockholm"));
+               
+                fragor.Add(new QuestionChoice("Vad heter Sveriges7 huvudstad?","1", "Stockholm", "Lissabon", "Oslo"));
+              
+                shuffle();
+
+                foreach (var fraga in fragor)
+                {
+                    fraga.Skrivut();
+                    fraga.GetAnswer(fraga);
+
+                }
+
+
+
+
+                
+                
         }
+
+        static void shuffle()
+        {
+            
+            Random random = new Random();
+            int tal1;
+            int tal2;
+
+            for(int i = 0; i < 10; i=i+1)
+            {
+            tal1 = random.Next(fragor.Count);
+            tal2 = random.Next(fragor.Count);
+            Questions12 temp = fragor [tal1];
+            fragor [tal1] = fragor [tal2];
+            fragor [tal2] = temp;
+            }
+
+        }
+
     }
+
+
+
+   
+        
+
 }
