7654ba7a5a054d5a01a1a9ce1c150d535e764110
arebrinkemil <earebrink@gmail.com>
2020-11-02 09:07:46 +0100

    qna

---
+++ QnA arv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QnA_arv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
