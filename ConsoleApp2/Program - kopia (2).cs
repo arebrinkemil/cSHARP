e8a222ce7e7ca7a672ea6e2538e1bd6f06623229
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2020-11-20 11:19:03 +0100

    Add files via upload

---
+++ ConsoleApp2/Program - kopia (2).cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UppgiftAnställd
+{
+    class UserInterfaces
+    {
+        static Personalhanterare ph = new Personalhanterare();
+
+        public void Run(){
+            PrintMenu();
+            String svar = Console.ReadLine();
+            while (svar != "a")
+            {
+                if (svar == "1")
+                {
+                    Console.WriteLine("");
+                    ph.LäggTillAnställd();
+                    Console.WriteLine("");                }
+
+                else if (svar =="2")
+                {
+                   Console.WriteLine(""); 
+                   ph.PrintAnställd(); 
+                }
+
+                else if (svar =="3")
+                {
+                    Console.WriteLine("");
+                    ph.Printnamnet();
+                    Console.WriteLine("");
+                }
+
+                else
+                {
+                    Console.WriteLine("Fel inmatning!");
+                }
+
+                PrintMenu();
+                svar = Console.ReadLine();
+            
+            }
+        }
+        public void PrintMenu()
+        {
+            Console.WriteLine("Välkommen");
+            Console.WriteLine("1. Lägg till en ny anställd");
+            Console.WriteLine("2. Skriv ut uppgifter om alla anställda");
+            Console.WriteLine("3. Skriv ut namn på alla anställda");
+            Console.WriteLine("a. Avsluta");
+        }
+    }
+
+     class Test
+    {
+        private List<Anställd> anställda = new List<Anställd>();
+        
+
+        public void Run()
+        {
+            anställda.Add(new Provisionsanställd("Ada", 6, 200000));
+            anställda.Add(new Timanställd("Gurra", 120, 35));
+            anställda.Add(new BasOchProvisionsanställd("Beda", 8, 150000, 10000));
+            anställda.Add(new Fastanställd("Kalle", 22000));
+
+            foreach (Anställd anstalld in anställda)
+            {
+                Console.WriteLine(anstalld.GetType().Name);
+                Console.WriteLine("");
+                Console.WriteLine(anstalld.ToString());
+                Console.WriteLine("");
+            }
+        }
+    }
+
+
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            test.Run();
+
+            UserInterfaces ui = new UserInterfaces();
+            ui.Run();
+        }
+    }
+}
---
+++ ConsoleApp2/Program - kopia (3).cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UppgiftAnställd
+{
+
+    abstract class Anställd
+    {
+        protected String namn;
+
+        public String Namn
+        {
+            get
+            {
+                return namn;
+            }
+        }
+
+        public Anställd(String namnet)
+        {
+            namn = namnet;
+        }
+
+        public abstract double BeräknaLön();
+
+        public override String ToString()
+        {
+            return namn + Environment.NewLine + BeräknaLön();
+        }
+
+    }
+
+    class Timanställd : Anställd
+    {
+        private double timlön;
+
+        private double arbetadeTimmar;
+
+        public Timanställd(String namnet, double timlönen, double timmar) :base(namnet)
+        {
+            timlön = timlönen;
+            arbetadeTimmar = timmar;
+        }
+
+        public override double BeräknaLön()
+        {
+            return timlön * arbetadeTimmar;
+        }
+
+        public override String ToString()
+        {
+            return base.ToString() + " = Månadslön"+ Environment.NewLine +  "Timlön: " + timlön + Environment.NewLine + "Arbetade timmar: " + arbetadeTimmar;
+        }
+    }
+
+
+    class Provisionsanställd : Anställd
+    {
+        protected double Provision;
+
+        protected double Försäljning;
+
+        public Provisionsanställd(String namnet, double provision, double försäljning) : base(namnet)
+        {
+            Provision = provision;
+            Försäljning = försäljning;
+
+        }
+
+        public override double BeräknaLön()
+        {
+            return (Provision/100) * Försäljning;
+        }
+
+        public override String ToString()
+        {
+            return base.ToString()+ " = Månadslön" + Environment.NewLine + "Provision: " + Provision + Environment.NewLine + "Försäljning: " + Försäljning; 
+            
+        }
+    }
+
+    class Fastanställd : Anställd
+    {
+        private double månadslön;
+
+        public Fastanställd(String namnet, double lön) : base(namnet)
+        {
+            månadslön = lön;
+        }
+
+        public override double BeräknaLön()
+        {
+            return månadslön;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " = Månadslön";
+        }
+    }
+
+     class BasOchProvisionsanställd : Provisionsanställd
+    {
+        private double fastLön;
+
+        public BasOchProvisionsanställd(String namnet, double provision, double försäljning, double lön) : base(namnet,provision,försäljning)
+        {
+            fastLön = lön;
+        }
+
+        public override double BeräknaLön()
+        {
+            return ((Provision/100) * Försäljning) + fastLön; 
+        }
+
+        public override String ToString()
+        {
+            return base.ToString() + Environment.NewLine + "Fast lön: " + fastLön;
+        }
+    }
+}
