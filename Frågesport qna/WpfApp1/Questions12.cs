e775f925fbe8ee3f4f8831b3934c8ca1fa21aeee
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2021-01-22 12:58:12 +0100

    frågesport

---
+++ Frågesport qna/WpfApp1/Questions12.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfDemoNestedStackPanel
+{
+    abstract class Questions12
+    {
+        protected String question;
+
+        protected String answer;
+
+
+        public string Question
+        {
+        get {
+                return question;
+
+            }
+        }
+
+        public string Answer
+        {
+            get
+            {
+                return answer;
+
+            }
+        }
+
+
+        public void GetAnswer(Questions12 svaret)
+        {
+            String SVAR = Console.ReadLine();
+            if (SVAR == svaret.answer)
+            {
+                Console.WriteLine("Rätt!");
+            }
+
+            else
+            {
+                Console.WriteLine("Fel!");
+            }
+
+        }
+        abstract public void Skrivut();
+    }
+
+
+   
+    
+
+
+
+    class QuestionText : Questions12
+    {
+
+        public QuestionText(String q1, String q2)
+        {
+            question = q1;
+            answer = q2;
+        }
+
+        public override void Skrivut()
+        {
+            Console.WriteLine(question);
+        }
+    }
+
+}
+
